b20a97615499418dacc2e199bec23fda42fa854d
Rodger <rodger0531@gmail.com>
2021-05-21 15:01:58 +0800

    fetch all episode data

--- src/episodes/index.js
+++ src/episodes/index.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useQuery, gql } from "@apollo/client";
+import { EpisodesList } from "./episodes-list";
 
 const GET_EPISODES = gql`
-  {
-    episodes {
+  query ($page: Int) {
+    episodes(page: $page) {
+      info {
+        next
+      }
       results {
         name
         episode
@@ -11,11 +15,41 @@ const GET_EPISODES = gql`
     }
   }
 `;
-const EpisodePage = () => {
-  const { loading, error, data, fetchMore } = useQuery(GET_EPISODES);
-  console.log(data);
 
-  return <div>episodes</div>;
+const EpisodePage = () => {
+  const { loading, error, data, fetchMore } = useQuery(GET_EPISODES, {
+    variables: {
+      page: 1,
+    },
+  });
+
+  useEffect(() => {
+    if (data?.episodes.info.next) {
+      fetchMore({
+        variables: { page: data.episodes.info.next },
+        updateQuery: (prev, { fetchMoreResult, variables }) => {
+          return {
+            episodes: {
+              info: fetchMoreResult.episodes.info,
+              results: [
+                ...prev.episodes.results,
+                ...fetchMoreResult.episodes.results,
+              ],
+            },
+          };
+        },
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data]);
+
+  return (
+    <div className="h-full w-full">
+      <EpisodesList></EpisodesList>
+      {/* {!data?.episodes.info.next &&
+        data.episodes.results.map((episode) => <p>{episode.name}</p>)} */}
+    </div>
+  );
 };
 
 export default EpisodePage;
