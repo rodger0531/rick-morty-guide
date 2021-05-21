3c0ad2a581bce77534a09256039b17fb0c3cbddc
Rodger <rodger0531@gmail.com>
2021-05-21 17:26:05 +0800

    add video to home

--- src/home.js
+++ src/home.js
@@ -1,5 +1,26 @@
-import React from "react";
-
+import React, { useRef } from "react";
+import video from "./homeBackground.mp4";
 export const Home = () => {
-  return <div>Welcome!</div>;
+  const videoRef = useRef(null);
+  const mute = () => {
+    console.log("🚀 ~ file: home.js ~ line 7 ~ mute ~ videoRef", videoRef);
+    videoRef.current.muted = !videoRef.current.muted;
+  };
+
+  return (
+    <>
+      <video
+        ref={videoRef}
+        src={video}
+        type="video/mp4"
+        className="w-screen w-auto"
+        autoPlay
+        muted
+        loop
+      />
+      <button type="button" onClick={mute}>
+        Mute
+      </button>
+    </>
+  );
 };
