5b3ffcb1f60125258b298bf470dba221b243c904
Rodger <rodger0531@gmail.com>
2021-05-21 11:22:08 +0800

    add responsive layout

--- src/characters/character-list.js
+++ src/characters/character-list.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useQuery, gql } from "@apollo/client";
-import { Controls } from "./controls";
+import { BottomControls } from "./bottom-controls";
 import { CharacterItem } from "./character-item";
-import { Filter } from "./filter";
-import { Skeleton, Space } from "antd";
+import { TopControls } from "./top-controls";
+import { Skeleton } from "antd";
 
 const GET_CHARACTERS = gql`
   query GET_CHARACTERS($page: Int, $species: String) {
@@ -33,28 +33,25 @@ const GET_CHARACTERS = gql`
 const CharacterList = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [filterSpecies, setFilterSpecies] = useState("");
-  // const [list, setList] = useState();
+
   const { loading, error, data } = useQuery(GET_CHARACTERS, {
     variables: { page: currentPage, species: filterSpecies },
   });
-  console.log(data);
-  // useEffect(() => {
-  //   data && setList(data.characters.results);
-  // }, [data]);
+
   return (
     <div className="h-full flex flex-col justify-between">
-      <Filter
+      <TopControls
         setFilterSpecies={setFilterSpecies}
         setCurrentPage={setCurrentPage}
       />
       <div className="">
         {error && <p>`Error! ${error.message}`</p>}
-        <div className="grid grid-cols-5 gap-6">
+        <div className="grid grid-cols-2 md:grid-cols-5 gap-6">
           {loading &&
             Array(20)
               .fill(0)
               .map((_, idx) => (
-                <div key={`${idx}`} className="w-28 lg:w-36 h-42">
+                <div key={`${idx}`} className="w-28 xl:w-36 h-42">
                   <Skeleton.Avatar
                     size={144}
                     className="rounded-xl"
@@ -64,13 +61,13 @@ const CharacterList = () => {
                 </div>
               ))}
           {data?.characters.results.map((item) => (
-            <div key={item.id} className="w-28 lg:w-36">
+            <div key={item.id} className="w-28 xl:w-36">
               <CharacterItem data={item} />
             </div>
           ))}
         </div>
       </div>
-      <Controls
+      <BottomControls
         info={data?.characters.info}
         loading={loading}
         setCurrentPage={setCurrentPage}
